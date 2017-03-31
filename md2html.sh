4c577ba222bfc8eff1454e22e4a8cf14908d4f60
CurtisWoo <curtwoo@gmail.com>
2017-04-01 08:04:57 +0900

    Added counter to show number of files converted.

--- md2html.sh
+++ md2html.sh
@@ -7,12 +7,14 @@ last=$BASH_ARGV
 
 if [ "$#" -ge 2 ] && [ -d "$last" ]
 then
+    count=0
     for file in "${files[@]}"; do
         if [[ "$file" == *".md" ]]; then
         pandoc "$file" --highlight-style=kate --template=foundation.html5 -o "$last/${file%.md}.html"
+        count=$((count+1))
         fi
     done
-    echo "Completed."
+    echo "Converted "$count" files."
 else
     echo "$USAGE"
     exit 1
