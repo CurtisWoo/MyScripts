47f4715e44ec38b48a58e715cd7ff41bf4acff5d
CurtisWoo <curtwoo@gmail.com>
2017-04-15 06:14:33 +0900

    Put dmenu on bottom and enlarged fonts.

--- listgitrepos.sh
+++ listgitrepos.sh
@@ -8,7 +8,7 @@ while IFS= read -d $'\0' -r file ; do
     repolist+="$dir\n"
 done < <(find ~ -type d -name .git -print0)
 
-choice=$(echo -e "$repolist" | dmenu)
+choice=$(echo -e "$repolist" | dmenu -b -fn "DejaVu Sans Mono 12")
 if [ -n "$choice" ]
 then 
     termite -d "${gitdic["$choice"]}" &
